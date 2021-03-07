c5f4be79fc15c89e96bd048fab77520107156acf
Pol Feliu <feliupol@gmail.com>
2021-03-07 11:45:25 +0100

    Added different types of fields

--- cyanochew/mainWindow.py
+++ cyanochew/mainWindow.py
@@ -55,29 +55,135 @@ class Window(QtWidgets.QMainWindow):
         for group,groupdata in data['properties'].items():
             if group == "info":
                 for name, fielddata in groupdata['properties'].items():
-                    self.expandFields(name, fielddata, self.info, "info")
-
-        print(self.dataHandles)
-
-
-    def expandFields(self, name, obj, parent, basename):
-        handlename = f'{basename}.{name}'
-        if 'type' not in obj or obj['type'] == 'string':
-            if 'description' in obj:
-                description = obj['description']
+                    self.expandField(name, fielddata, self.info, "info")
+            elif group == "i2c":
+                for name, fielddata in groupdata['properties'].items():
+                    self.expandField(name, fielddata, self.I2C, "I2C")
+            elif group == "spi":
+                for name, fielddata in groupdata['properties'].items():
+                    self.expandField(name, fielddata, self.SPI, "SPI")
+            elif group == "registers":
+                self.loadRegisters()
+            elif group == "fields":
+                self.loadFields()
+            elif group == "functions":
+                self.loadFunctions()
+            elif group == "extensions":
+                pass
+            elif group == "cyanobyte":
+                pass
             else:
-                description = ""
+                print(group)
 
-            lineedit = QtWidgets.QLineEdit()
-            lineedit.setToolTip(description)
-            self.dataHandles[handlename] = lineedit
-            parent.addRow(name, lineedit)
+        #pprint(self.dataHandles)
 
+    def loadRegisters(self):
+        pass
+
+    def loadFields(self):
+        pass
+
+    def loadFunctions(self):
+        pass
+
+    def createRadioField(self, name, description,  obj, parent, basename):
+        groupbox = QtWidgets.QGroupBox()
+        self.dataHandles[basename] = groupbox
+        groupboxlayout = QtWidgets.QVBoxLayout()
+        groupbox.setLayout(groupboxlayout)
+        groupbox.setToolTip(description)
+
+        for option in obj['enum']:
+            radiobutton = QtWidgets.QRadioButton(option)
+            groupboxlayout.addWidget(radiobutton)
+            handlename = f'{basename}.{option}'
+            self.dataHandles[handlename] = radiobutton
+
+        parent.addRow(name, groupbox)
+
+    def createCheckBoxField(self, name, description,  obj, parent, basename):
+        groupbox = QtWidgets.QGroupBox()
+        self.dataHandles[basename] = groupbox
+        groupboxlayout = QtWidgets.QVBoxLayout()
+        groupbox.setLayout(groupboxlayout)
+        groupbox.setToolTip(description)
+
+        for option in obj['enum']:
+            radiobutton = QtWidgets.QCheckBox(option)
+            groupboxlayout.addWidget(radiobutton)
+            handlename = f'{basename}.{option}'
+            self.dataHandles[handlename] = radiobutton
+
+        parent.addRow(name, groupbox)
+
+    def createLineField(self, name, description,  obj, parent, basename):
+        handlename = f'{basename}.{name}'
+
+        lineedit = QtWidgets.QLineEdit()
+        lineedit.setToolTip(description)
+        self.dataHandles[handlename] = lineedit
+        parent.addRow(name, lineedit)
+
+    def createSpinBoxField(self, name, description, obj, parent, basename, type='double'):
+        handlename = f'{basename}.{name}'
+        if type == 'double':
+            spinbox = QtWidgets.QDoubleSpinBox()
+        elif type == 'integer':
+            spinbox = QtWidgets.QSpinBox()
+        spinbox.setToolTip(description)
+        self.dataHandles[handlename] = spinbox
+        parent.addRow(name, spinbox)
+
+    def expandField(self, name, obj, parent, basename):
+        handlename = f'{basename}.{name}'
+
+        if 'description' in obj:
+            description = obj['description']
+        else:
+            description = ""
+
+        if 'enum' in obj:
+            self.createRadioField(
+                name,
+                description,
+                obj,
+                parent,
+                handlename
+            )
+        elif 'anyOf' in obj:
+            for data in obj['anyOf']:
+                if 'enum' in data:
+                    self.createCheckBoxField(
+                        name,
+                        description,
+                        data,
+                        parent,
+                        handlename
+                    )
+        elif 'type' not in obj or obj['type'] == 'string':
+            self.createLineField(name, description, obj, parent, handlename)
+        elif obj['type'] == 'number':
+            self.createSpinBoxField(
+                name,
+                description,
+                obj,
+                parent,
+                basename,
+            )
+        elif obj['type'] == 'integer':
+            self.createSpinBoxField(
+                name,
+                description,
+                obj,
+                parent,
+                basename,
+                type='integer'
+            )
         elif obj['type'] == 'object':
             form = QtWidgets.QFormLayout()
             parent.addRow(name,form)
             for childname, childfielddata in obj['properties'].items():
-                self.expandFields(childname, childfielddata, form, handlename)
+                self.expandField(childname, childfielddata, form, handlename)
         else:
             print(name + "##############################")
             pprint(obj)
