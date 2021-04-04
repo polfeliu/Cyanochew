8daa33a16a2fd1906766aaaefb6063f11c47af35
Pol Feliu <plo.fc.123@gmail.com>
2021-04-04 19:32:50 +0200

    Ported from ply to sly lexer/parser. It is more modern and the syntax is very similar. The parser syntax is defined with decorators instead of docstrings, it is more elegant IMO.

--- cyanochew/FunctionParser.py
+++ cyanochew/FunctionParser.py
@@ -1,164 +1,104 @@
-import ply.lex as lex
-import ply.yacc as yacc
-import sys
-import warnings
-warnings.simplefilter('always', UserWarning)
+from sly import Lexer, Parser
 
-class FunctionLexer(object):
-    # List of token names.   This is always required
-    tokens = (
-        'INT',
-        'FLOAT',
-        'NAME',
-        'PLUS',
-        'MINUS',
-        'DIVIDE',
-        'MULTIPLY',
-        'EQUALS'
-    )
+class FunctionLexer(Lexer):
+    tokens = {
+        'NAME', 'INT', 'FLOAT'
+        }
+    ignore = ' \t'
+    literals = { '=', '+', '-', '*', '/', '(', ')'}
 
-    # Regular expression rules for simple tokens
-    t_PLUS = r'\+'
-    t_MINUS = r'\-'
-    t_MULTIPLY = r'\*'
-    t_DIVIDE = r'\/'
-    t_EQUALS = r'\='
+    # Tokens
+    NAME = r'[a-zA-Z_][a-zA-Z0-9_]*'
 
-    def t_FLOAT(self,t):
-        r'\d+\.\d+'
+    @_(r"\d+\.\d*")
+    def FLOAT(self, t):
         t.value = float(t.value)
         return t
 
-    def t_INT(self,t):
-        r'\d+'
+    @_(r'\d+')
+    def INT(self, t):
         t.value = int(t.value)
         return t
 
-    def t_NAME(self,t):
-        r'[a-zA-Z_][a-zA-Z_0-9]*'
-        t.type = 'NAME'
+    @_(r'\n+')
+    def newline(self, t):
+        self.lineno += t.value.count('\n')
+
+    def NAME(self, t):
+        t.value = t.value.upper()
         return t
 
-    # A string containing ignored characters (spaces and tabs)
-    t_ignore = ' \t'
+    def error(self, value):
+        print("Illegal character '%s' at line %s " % (value.value, value.lineno))
+        self.index += 1
 
-    # Error handling rule
-    def t_error(self, t):
-        print("Illegal character '%s'" % t.value[0])
-        t.lexer.skip(1)
-
-    # Build the lexer
-    def build(self, **kwargs):
-        self.lexer = lex.lex(module=self, **kwargs)
-
-    def __init__(self):
-        self.build()
-
-    # Test it output
-    def test(self, data):
-        self.lexer.input(data)
-        while True:
-            tok = self.lexer.token()
-            if not tok:
-                break
-            print(tok)
-
-class FunctionParser():
-
+class FunctionParser(Parser):
     tokens = FunctionLexer.tokens
 
     precedence = (
-        ('left', 'PLUS', 'MINUS'),
-        ('left', 'MULTIPLY', 'DIVIDE')
-    )
-
-    def p_calc(self, p):
-        '''
-        calc : expression
-             | var_assign
-             | empty
-        '''
-        print(self.run(p[1]))
-
-    def p_var_assign(self, p):
-        '''
-        var_assign : NAME EQUALS expression
-        '''
-        p[0] = ('=', p[1], p[3])
-
-    def p_expression(self, p):
-        '''
-        expression : expression MULTIPLY expression
-        | expression DIVIDE expression
-        | expression PLUS expression
-        | expression MINUS expression
-        '''
-        p[0] = (p[2], p[1], p[3])
-
-    def p_expression_int_float(self, p):
-        '''
-        expression : INT
-                    | FLOAT
-        '''
-        p[0] = p[1]
-
-    def p_expression_var(self, p):
-        '''
-        expression : NAME
-        '''
-        p[0] = ('var', p[1])
-
-    def p_empty(self, p):
-        '''
-        empty :
-        '''
-        p[0] = None
-
-    def p_error(self, p):
-        print("Parsing error")
-
-    env = {}
-    def run(self, p):
-        if type(p) == tuple:
-            if len(p) >= 1:
-                if p[1] is None:
-                    return None
-            elif len(p) >= 2:
-                if p[2] is None:
-                    return None
-
-            if p[0] == '+':
-                return self.run(p[1]) + self.run(p[2])
-            elif p[0] == '-':
-                return self.run(p[1]) - self.run(p[2])
-            elif p[0] == '*':
-                return self.run(p[1]) * self.run(p[2])
-            elif p[0] == '/':
-                return self.run(p[1]) / self.run(p[2])
-            elif p[0] == '=':
-                self.env[p[1]] = self.run(p[2])
-            elif p[0] == 'var':
-                if p[1] not in self.env:
-                    warnings.warn('Undeclared Variable')
-                    return None
-                else:
-                    return self.env[p[1]]
-        else:
-            return p
+        ('left', '+', '-'),
+        ('left', '*', '/'),
+        ('right', 'UMINUS'),
+        )
 
     def __init__(self):
-        print("building")
-        self.lexer = FunctionLexer()
-        self.parser = yacc.yacc(module=self)
+        self.names = { }
 
-    def parse(self, p):
-        self.parser.parse(p)
+    @_('NAME "=" expr')
+    def statement(self, p):
+        self.names[p.NAME] = p.expr
 
-p = FunctionParser()
+    @_('expr')
+    def statement(self, p):
+        print(p.expr)
 
-while True:
-    try:
-        s = input('>>')
-    except EOFError:
-        break
-    p.parse(s)
+    @_('expr "+" expr')
+    def expr(self, p):
+        return p.expr0 + p.expr1
+
+    @_('expr "-" expr')
+    def expr(self, p):
+        return p.expr0 - p.expr1
+
+    @_('expr "*" expr')
+    def expr(self, p):
+        return p.expr0 * p.expr1
+
+    @_('expr "/" expr')
+    def expr(self, p):
+        return p.expr0 / p.expr1
+
+    @_('"-" expr %prec UMINUS')
+    def expr(self, p):
+        return -p.expr
+
+    @_('"(" expr ")"')
+    def expr(self, p):
+        return p.expr
+
+    @_('INT')
+    def expr(self, p):
+        return p.INT
+
+    @_('FLOAT')
+    def expr(self, p):
+        return p.FLOAT
+
+    @_('NAME')
+    def expr(self, p):
+        try:
+            return self.names[p.NAME]
+        except LookupError:
+            print("Undefined name '%s'" % p.NAME)
+            return 0
+
+if __name__ == '__main__':
+    lexer = FunctionLexer()
+    parser = FunctionParser()
+    while True:
+        try:
+            text = input('calc > ')
+        except EOFError:
+            break
+        if text:
+            parser.parse(lexer.tokenize(text))
