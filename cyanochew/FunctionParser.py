f214ae0bb19f9fb03a09d5f547f8d4fc85382bed
Pol Feliu <feliupol@gmail.com>
2021-04-17 13:58:58 +0200

    created a simple syntax tree

--- cyanochew/FunctionParser.py
+++ cyanochew/FunctionParser.py
@@ -32,16 +32,39 @@ class FunctionLexer(Lexer):
         self.lineno += t.value.count('\n')
 
     def TYPE(self, t):
-        t.value = t.value
         return t
 
     def NAME(self, t):
-        t.value = t.value.upper()
         return t
 
     def error(self, value):
         print("Illegal character '%s' at line %s " % (value.value, value.lineno))
         self.index += 1
+
+
+class Function:pass
+class Functiondef:pass
+class Comment:pass
+class Return:pass
+class Sum:pass
+class Difference:pass
+class Product:pass
+class Division:pass
+class Power:pass
+class Modulus:pass
+class BitwiseOr:pass
+class BitwiseAnd:pass
+class BitshitLeft:pass
+class BitshitRight:pass
+class ArcTangent:pass
+class Declaration:pass
+class AssignmentDeclaration:pass
+class Assignment:pass
+class CmdWrite:pass
+class RawRead:pass
+class Delay:pass
+class Number:pass
+class Variable:pass
 
 class FunctionParser(Parser):
     tokens = FunctionLexer.tokens
@@ -57,13 +80,13 @@ class FunctionParser(Parser):
     def __init__(self):
         self.names = { }
 
-    @_(' { NEWLINE } { function } { NEWLINE } ')
-    def program(self, p):
-        pass
-
-    @_('functiondef "{"  { NEWLINE } statements NEWLINE { NEWLINE } returndef { NEWLINE } "}" ')
+    @_(' { NEWLINE } functiondef "{"  { NEWLINE } statements NEWLINE { NEWLINE } returndef { NEWLINE } "}" { NEWLINE } ')
     def function(self,p):
-        pass
+        f = Function()
+        f.functiondef = p.functiondef
+        f.statements = p.statements
+        f.returndef = p.returndef
+        return f
 
     # Statement
     @_('statements NEWLINE { NEWLINE } statement')
@@ -75,79 +98,126 @@ class FunctionParser(Parser):
     def statements(self, p):
         return [p.statement]
 
-
     # Function declaration
     @_('FUN NAME "(" NAME  ":" TYPE { "," NAME  ":" TYPE } ")" ')
     def functiondef(self, p):
-        print(p.NAME1, p.TYPE0, p.NAME2, p.TYPE1)
+        f = Functiondef()
+        f.name = p.NAME0
+        f.variables = {
+            p.NAME1 : p.TYPE0
+        }
+        for i in range(len(p.NAME2)):
+            name = p.NAME2[i]
+            type = p.TYPE1[i]
+            f.variables[name] = type
+
+        return f
 
     # Function return variable
     @_('RETURN NAME')
     def returndef(self, p):
-        print(p)
+        r = Return()
+        r.variables = [p.NAME]
 
     # Function return list of variables
     @_('RETURN "[" NAME { "," NAME } "]"')
-    def statement(self, p):
-        print(p.NAME0, p.NAME1)
+    def returndef(self, p):
+        r = Return()
+        r.variables = [p.NAME0] + p.NAME1
 
     # Empty declaration
     @_('NAME ":" TYPE')
     def statement(self, p):
-        print(p)
+        d = Declaration()
+        d.variable = p.NAME
+        d.type = p.TYPE
+        return d
 
     # Declaration with assignment
     @_('NAME ":" TYPE "=" expr')
     def statement(self, p):
+        a = AssignmentDeclaration()
+        a.variable = p.NAME
+        a.type = p.TYPE
+        a.expression = p.expr
         print(p.expr)
 
     # Reassignment without declaration
     @_('NAME "=" expr')
     def statement(self, p):
-        self.names[p.NAME] = p.expr
+        a = Assignment()
+        a.variable = p.NAME
+        a.expression = p.expr
+        return a
 
     # cmdWrite
     @_(' REGISTER NAME ARROW NAME')
     def statement(self, p):
-        print("cmdWrite")
+        c = CmdWrite()
+        c.register = p.NAME0
+        c.variable = p.NAME1
+        return c
 
     # rawRead
     @_(' NAME ARROW REGISTER NAME')
     def statement(self, p):
-        print("cmdRead")
+        r = RawRead()
+        r.register = p.NAME1
+        r.variable = p.NAME0
+        return r
 
     # delay
     @_('delaydef "{" { NEWLINE } statements { NEWLINE } "}" ')
     def statement(self, p):
-        print("qwer")
+        d = Delay()
+        d.delay = p.delaydef
+        d.body = p.statements
+        return d
 
     @_('DELAY INT')
     def delaydef(self, p):
-        print("delay")
+        return int(p.INT)
 
     @_('DELAY FLOAT ')
     def delaydef(self, p):
-        print("delay")
+        return float(p.FLOAT)
 
     @_('expr "+" expr')
     def expr(self, p):
-        return p.expr0 + p.expr1
+        d = Difference()
+        d.summand0 = p.expr0
+        d.summand1 = p.expr1
+        return d
 
     @_('expr "-" expr')
     def expr(self, p):
-        return p.expr0 - p.expr1
+        d = Difference()
+        d.minuend = p.expr0
+        d.subtrahend = p.expr1
+        return d
 
     @_('expr "*" expr')
     def expr(self, p):
-        return p.expr0 * p.expr1
+        p = Product()
+        p.factor0 = p.expr0
+        p.factor1 = p.expr1
+        return p
 
     @_('expr "/" expr')
     def expr(self, p):
-        return p.expr0 / p.expr1
+        d = Division()
+        d.divident = p.expr0
+        d.divisor = p.expr1
+        return d
 
     @_('"-" expr %prec UMINUS')
     def expr(self, p):
-        return -p.expr
+        d = Difference()
+        n0 =  Number()
+        n0.value = 0
+        d.minuend = n0
+        d.subtrahend = p.expr
+        return d
 
     @_('"(" expr ")"')
     def expr(self, p):
@@ -155,19 +225,21 @@ class FunctionParser(Parser):
 
     @_('INT')
     def expr(self, p):
-        return p.INT
+        n = Number()
+        n.value = p.INT
+        return n
 
     @_('FLOAT')
     def expr(self, p):
-        return p.FLOAT
+        n = Number()
+        n.value = p.FLOAT
+        return n
 
     @_('NAME')
     def expr(self, p):
-        try:
-            return self.names[p.NAME]
-        except LookupError:
-            print("Undefined name '%s'" % p.NAME)
-            return 0
+        v = Variable()
+        v.name = p.NAME
+        return v
 
 if __name__ == '__main__':
     lexer = FunctionLexer()
@@ -178,8 +250,8 @@ if __name__ == '__main__':
         except EOFError:
             break
         if text:
-            print([tok for tok in lexer.tokenize(text)])
-            #p = parser.parse(lexer.tokenize(text))
-            print(parser.log)
+            #print([tok for tok in lexer.tokenize(text)])
+            p = parser.parse(lexer.tokenize(text))
+            print(p)
 
         break
